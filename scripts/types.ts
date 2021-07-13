314ea2539951a87ca41552e314c1f27b16c26505
Kevin Loeffler <hi@kevinloeffler.ch>
2021-07-13 12:55:21 +0200

    Added Levels and MAXSPEED

--- scripts/types.ts
+++ scripts/types.ts
@@ -1,5 +1,6 @@
 // Game Constants
 let SPEED = 600
+let MAXSPEED = 100
 const BOARDSIZE = 12
 
 enum Dir {
@@ -9,13 +10,28 @@ enum Dir {
     Left = 'Left'
 }
 
+const LEVELS = {
+    0: {'name': 'Beginner', 'startSpeed': 1000, 'maxSpeed': 200},
+    1: {'name': 'Easy', 'startSpeed': 800, 'maxSpeed': 140},
+    2: {'name': 'Medium', 'startSpeed': 600, 'maxSpeed': 140},
+    3: {'name': 'Advanced', 'startSpeed': 400, 'maxSpeed': 120},
+    4: {'name': 'Hard', 'startSpeed': 300, 'maxSpeed': 100},
+    5: {'name': 'Maniac', 'startSpeed': 200, 'maxSpeed': 80},
+}
+
 function reduceSpeed (): void {
-    SPEED = Math.floor(SPEED * 0.9)
-    console.log('New Speed: ' + SPEED)
+    if (SPEED > MAXSPEED) {
+        SPEED = Math.floor(SPEED * 0.9)
+        console.log('New Speed: ' + SPEED)
+    }
 }
 
 function setSpeed (newSpeed: number) {
     SPEED = newSpeed
 }
 
-export {Dir, BOARDSIZE, SPEED, reduceSpeed, setSpeed}
+function setMaxSpeed (newSpeed: number) {
+    MAXSPEED = newSpeed
+}
+
+export {Dir, LEVELS, BOARDSIZE, SPEED, MAXSPEED, reduceSpeed, setSpeed, setMaxSpeed}
